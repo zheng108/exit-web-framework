b4d7df43f6fd559cac3ab40ef8f2413284f735b4
exitsoft <exitsoft@vip.qq.com>
2012-08-31 20:33:44 -0700

    增强ShiroDataBaseRealm授权和验证和添加shiro-cas包。
    1.添加defaultPermissions和defaultRoles属性，可以通过spring配置文件直接定义
    2.修改数据库表tb_resource的permission字段，可以同过逗号分割多个过滤器
    3.修改认证方法，通过Group实体转变为SimpleAuthorizationInfo的role属性，
    4.将Gorup添加两个字段role,value。来做SimpleAuthorizationInfo的role描述

--- showcase/vcs_admin/src/main/java/org/exitsoft/showcase/vcsadmin/service/account/ShiroDataBaseRealm.java
+++ showcase/vcs_admin/src/main/java/org/exitsoft/showcase/vcsadmin/service/account/ShiroDataBaseRealm.java
@@ -1,7 +1,6 @@
 package org.exitsoft.showcase.vcsadmin.service.account;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
