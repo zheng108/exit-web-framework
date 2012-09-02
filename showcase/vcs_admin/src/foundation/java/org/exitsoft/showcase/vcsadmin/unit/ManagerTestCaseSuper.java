385802a129d8a4abde5882ca3c2f48afc10d2984
exitsoft <exitsoft@vip.qq.com>
2012-09-01 20:11:10 -0700

    #9问题修复

--- showcase/vcs_admin/src/foundation/java/org/exitsoft/showcase/vcsadmin/unit/ManagerTestCaseSuper.java
+++ showcase/vcs_admin/src/foundation/java/org/exitsoft/showcase/vcsadmin/unit/ManagerTestCaseSuper.java
@@ -24,6 +24,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("/applicationContext-test.xml")
 public class ManagerTestCaseSuper {
 	
+	
 	protected DataSource dataSource;
 	
 	protected NamedParameterJdbcTemplate jdbcTemplate;
@@ -43,10 +44,23 @@ public class ManagerTestCaseSuper {
 		this.sessionFactory = sessionFactory;
 	}
 
+	/**
+	 * 通过表名计算出表中的总记录数
+	 * 
+	 * @param tableName 表名
+	 * 
+	 * @return int
+	 */
 	protected int countRowsInTable(String tableName) {
 		return jdbcTemplate.queryForInt("SELECT COUNT(0) FROM " + tableName,new HashMap<String, Object>());
 	}
 	
+	/**
+	 * 
+	 * 每个单元测试用例开始先把模拟数据加载到dataSource中，如果整个单元测试类（不是单个方法用例，是整个类）已经存在数据，就不重复加载
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void reloadSampleData() throws Exception {
 		if (dataSourceHandler == null) {
@@ -55,7 +69,11 @@ public class ManagerTestCaseSuper {
 		}
 
 	}
-
+	
+	/**
+	 * 整个类的单元测试方法用例测试完成后，将dataSource的模拟数据清楚，让reloadSampleData方法在重新加载模拟数据，供第二个单元测试类使用
+	 * @throws Exception
+	 */
 	@AfterClass
 	public static void cleanData() throws Exception {
 		Fixtures.deleteData(dataSourceHandler, "/sample-data.xml");
